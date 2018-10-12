ac77b3f638323ef944844c78f7fb9d5b86b2a995
luke-goldfain <lukegoldfain@gmail.com>
2018-10-12 12:29:04 -0500

    asdfasdf

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -15,19 +15,74 @@ public class TestInput : MonoBehaviour {
     {
 
 
-        if (Input.GetButtonDown("joy1A"))
+        if (Joy1A())
         {
             Debug.Log("joystick 1 A pressed");
         }
 
-        if (Input.GetButtonDown("joy1B"))
+        if (Joy1B())
         {
             Debug.Log("joystick 1 B pressed");
         }
 
-        if (Input.GetButtonDown("joy1X"))
+        if (Joy1X())
         {
             Debug.Log("joystick 1 X pressed");
         }
+
+        if (Joy1Y())
+        {
+            Debug.Log("joystick 1 y pressed");
+        }
+
+        if (Joy1Start())
+        {
+            Debug.Log("joystick 1 start pressed");
+        }
+
+        if (Joy2A())
+        {
+            Debug.Log("joystick 2 A pressed");
+        }
+
+        if (Joy2Start())
+        {
+            Debug.Log("joystick 2 start pressed");
+        }
+    }
+
+    private bool Joy1A()
+    {
+        return Input.GetButtonDown("joy1A");
+    }
+
+    private bool Joy1B()
+    {
+        return Input.GetButtonDown("joy1B");
+    }
+
+    private bool Joy1X()
+    {
+        return Input.GetButtonDown("joy1X");
+    }
+
+    private bool Joy1Y()
+    {
+        return Input.GetButtonDown("joy1Y");
+    }
+
+    private bool Joy1Start()
+    {
+        return Input.GetButtonDown("joy1start");
+    }
+
+    private bool Joy2A()
+    {
+        return Input.GetButtonDown("joy2A");
+    }
+
+    private bool Joy2Start()
+    {
+        return Input.GetButtonDown("joy2start");
     }
 }
