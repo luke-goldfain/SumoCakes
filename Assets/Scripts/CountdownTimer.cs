a25dc022afad52b5e941922b06b62d77171147df
William Fergus <Will.Fergus@iam.local>
2018-10-12 16:01:36 -0500

    adds countdown script

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountdownTimer : MonoBehaviour {
+
+    public int timeLeft = 5;
+    public Text countdownText;
+	void Start ()
+    {
+        StartCoroutine("LoseTime");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        countdownText.text = ("" + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times up";
+        }
+	}
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
