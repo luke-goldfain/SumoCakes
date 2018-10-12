56dc31a329f142b0cc41ff25629b446520c93066
luke-goldfain <lukegoldfain@gmail.com>
2018-10-12 11:35:15 -0500

    Update Assets/Scripts/TestInput.cs

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scrip : MonoBehaviour {
+public class TestInput : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
