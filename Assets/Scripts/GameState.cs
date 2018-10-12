38b4db2d655ffc4cae5af16af54b63f56355bab4
adan.benitez <adan.benitez@loop.colum.edu>
2018-10-12 12:48:23 -0500

    Adan
    Art Addition, and Game State Manager

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates { Start, Playing, Pause, Loading, End}
+public class GameState : MonoBehaviour {
+
+    private GameStates currentGameState;
+
+    public GameStates _GameState {
+        get
+        {
+            return currentGameState;
+        }
+
+        private set
+        {
+            currentGameState = value;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateGameStates();
+
+	}
+
+    //used to switch the Scenes
+    private void UpdateGameStates()
+    {
+        switch (_GameState)
+        {
+            case GameStates.Start:
+                break;
+
+            case GameStates.Playing:
+                break;
+
+            case GameStates.Pause:
+                break;
+
+            case GameStates.Loading:
+                break;
+
+            case GameStates.End:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
