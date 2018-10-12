28b06ed78f49ebd8b17244e8d716ccbd1fd7c4d3
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2018-10-12 18:12:20 -0500

    Audio Manager Script
    This is the audio manager script which would help the programmers embed the audio anywhere with ease. There are 2 classes: AudioManager.cs and Sound.cs

---
+++ Assets/Sound Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿// Code used from Brackleys Introduction to Audio in Unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+[System.Serializable] //Used so we can see the options within Unity itself
+
+public class Sound //Class used to change factors of our audio source like volume, pitch etc. 
+
+{
+
+    public string name; //name of the sound
+
+    public AudioClip clip; //links the audio clip
+
+    public float volume; //change the volume
+    
+    public float pitch; //change the pitch
+
+    public bool loop; //if you want to loop the sound
+
+
+    [HideInInspector]
+    public AudioSource source; //the access variable of the audio source. We can call methods using this. 
+
+}
