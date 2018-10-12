046ecfc46ce5e1df6e22a2eaec876ce8a243c381
luke-goldfain <lukegoldfain@gmail.com>
2018-10-12 12:04:59 -0500

    jsnfldglksd

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -7,12 +7,27 @@ public class TestInput : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-		
+	    
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+
+
+        if (Input.GetButtonDown("joy1A"))
+        {
+            Debug.Log("joystick 1 A pressed");
+        }
+
+        if (Input.GetButtonDown("joy1B"))
+        {
+            Debug.Log("joystick 1 B pressed");
+        }
+
+        if (Input.GetButtonDown("joy1X"))
+        {
+            Debug.Log("joystick 1 X pressed");
+        }
+    }
 }
