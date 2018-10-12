6376a5c47c5c50d7669616fb9c68f51dc40c7233
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-12 15:50:23 -0500

    Add scene change script test

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	public void ChangeMenuScene(string sceneName)
+    {
+        SceneManager.LoadScene("ControlScene", LoadSceneMode.Single);
+    }
+}
