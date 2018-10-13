9fd0cdbd98ebc7340bd0ebd4c56d72870dc04f0a
luke-goldfain <lukegoldfain@gmail.com>
2018-10-12 19:28:16 -0500

    getting camera to work and other variables set again

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject canvas;
+
+    bool GameEnded;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameEnded = false;
+
+        canvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        canvas.SetActive(GameEnded);
+
+        // TODO: Set an image on canvas to reflect winner
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set camera's transform to be the same as the player's
+        transform.position = player.transform.position + offset;
+    }
+}
